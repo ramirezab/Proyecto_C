91fb1ccc6dcc98e4b3bdaf1cb426e19da81b24bd
ramirezab <bryanramirez1220@gmail.com>
2021-12-09 03:57:34 -0600

    Se limpia la ventana despuès de una victoria

--- ejemplo_gui.c
+++ ejemplo_gui.c
@@ -14,6 +14,16 @@ char *lb9 = "i";
 char *figura1 = "X";
 char *figura2 = "O";
 
+GtkButton *button_1;
+GtkButton *button_2;
+GtkButton *button_3;
+GtkButton *button_4;
+GtkButton *button_5;
+GtkButton *button_6;
+GtkButton *button_7;
+GtkButton *button_8;
+GtkButton *button_9;
+
 void swap(char **a, char **b) {
     char *temp = *a;
     *a = *b;
@@ -25,42 +35,54 @@ void set_char(char **a, char **b){
     *a = *b;
 
 }
+void clean_board();
+
 
 void condicion_victoria(){
 
     if (strcmp(lb1, lb2) == 0 && strcmp(lb2, lb3) == 0){
         g_print("Victoria de %s", figura1);
+        clean_board();
     }
     else if (strcmp(lb4, lb5) == 0 && strcmp(lb5, lb6) == 0){
         g_print("Victoria de %s", figura1);
+        clean_board();
     }
     else if (strcmp(lb7, lb8) == 0 && strcmp(lb8, lb9) == 0){
         g_print("Victoria de %s", figura1);
+        clean_board();
     }
     else if (strcmp(lb1, lb4) == 0 && strcmp(lb4, lb7) == 0){
         g_print("Victoria de %s", figura1);
+        clean_board();
     }
     else if (strcmp(lb2, lb5) == 0 && strcmp(lb5, lb8) == 0){
         g_print("Victoria de %s", figura1);
+        clean_board();
     }
     else if (strcmp(lb3, lb6) == 0 && strcmp(lb6, lb9) == 0){
         g_print("Victoria de %s", figura1);
+        clean_board();
     }
     else if (strcmp(lb1, lb5) == 0 && strcmp(lb5, lb9) == 0){
         g_print("Victoria de %s", figura1);
+        clean_board();
     }
     else if (strcmp(lb3, lb5) == 0 && strcmp(lb5, lb7) == 0){
         g_print("Victoria de %s", figura1);
+        clean_board();
     }
 
 
 
 }
 
+
+
 void on_clicked_1(GtkWidget *widget, gpointer data) {
 
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_1= (GtkButton*) widget;
+    button_1 = (GtkButton*) widget;
     gtk_button_set_label(button_1, figura1);
     set_char(&lb1, &figura1);
     g_print("%s \n",lb1);
@@ -72,7 +94,7 @@ void on_clicked_1(GtkWidget *widget, gpointer data) {
 
 void on_clicked_2(GtkWidget *widget, gpointer data) {
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_2= (GtkButton*) widget;
+    button_2= (GtkButton*) widget;
     gtk_button_set_label(button_2, figura1);
     set_char(&lb2, &figura1);
     g_print("%s \n",lb2);    
@@ -84,7 +106,7 @@ void on_clicked_2(GtkWidget *widget, gpointer data) {
 
 void on_clicked_3(GtkWidget *widget, gpointer data) {
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_3= (GtkButton*) widget;
+    button_3= (GtkButton*) widget;
     gtk_button_set_label(button_3, figura1);
     set_char(&lb3, &figura1);
     g_print("%s \n",lb3);
@@ -95,7 +117,7 @@ void on_clicked_3(GtkWidget *widget, gpointer data) {
 
 void on_clicked_4(GtkWidget *widget, gpointer data) {
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_4= (GtkButton*) widget;
+    button_4= (GtkButton*) widget;
     gtk_button_set_label(button_4, figura1);
     set_char(&lb4, &figura1);
     g_print("%s \n",lb4);
@@ -106,7 +128,7 @@ void on_clicked_4(GtkWidget *widget, gpointer data) {
 
 void on_clicked_5(GtkWidget *widget, gpointer data) {
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_5= (GtkButton*) widget;
+    button_5= (GtkButton*) widget;
     gtk_button_set_label(button_5, figura1);
     set_char(&lb5, &figura1);
     g_print("%s \n",lb5);
@@ -117,7 +139,7 @@ void on_clicked_5(GtkWidget *widget, gpointer data) {
 
 void on_clicked_6(GtkWidget *widget, gpointer data) {
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_6= (GtkButton*) widget;
+    button_6= (GtkButton*) widget;
     gtk_button_set_label(button_6, figura1);
     set_char(&lb6, &figura1);
     g_print("%s \n",lb6);
@@ -128,7 +150,7 @@ void on_clicked_6(GtkWidget *widget, gpointer data) {
 
 void on_clicked_7(GtkWidget *widget, gpointer data) {
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_7= (GtkButton*) widget;
+    button_7= (GtkButton*) widget;
     gtk_button_set_label(button_7, figura1);
     set_char(&lb7, &figura1);
     g_print("%s \n",lb7);
@@ -140,7 +162,7 @@ void on_clicked_7(GtkWidget *widget, gpointer data) {
 
 void on_clicked_8(GtkWidget *widget, gpointer data) {
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_8= (GtkButton*) widget;
+    button_8= (GtkButton*) widget;
     gtk_button_set_label(button_8, figura1);
     set_char(&lb8, &figura1);
     g_print("%s \n",lb8);
@@ -151,13 +173,36 @@ void on_clicked_8(GtkWidget *widget, gpointer data) {
 
 void on_clicked_9(GtkWidget *widget, gpointer data) {
     GtkLabel *label = (GtkLabel*) data;
-    GtkButton *button_9= (GtkButton*) widget;
+    button_9= (GtkButton*) widget;
     gtk_button_set_label(button_9, figura1);
     set_char(&lb9, &figura1);
     g_print("%s \n",lb9);
     g_print("Button 9 Clicked\n");
     condicion_victoria();
     swap(&figura1, &figura2);
+}
+
+void clean_board(){
+
+    gtk_button_set_label(button_1, " ");
+    gtk_button_set_label(button_2, " ");
+    gtk_button_set_label(button_3, " ");
+    gtk_button_set_label(button_4, " ");
+    gtk_button_set_label(button_5, " ");
+    gtk_button_set_label(button_6, " ");
+    gtk_button_set_label(button_7, " ");
+    gtk_button_set_label(button_8, " ");
+    gtk_button_set_label(button_9, " ");
+    lb1 = "a";
+    lb2 = "b";
+    lb3 = "c";
+    lb4 = "d";
+    lb5 = "e";
+    lb6 = "f";
+    lb7 = "g";
+    lb8 = "h";
+    lb9 = "i";
+        
 }
 
 int main(int argc, char* argv[]) {
